977385d0fd89d8f6e5bea2904c0094bad0664e06
mganeko <masashi.g@gmail.com>
2024-05-09 18:44:21 +0900

    try camera pos

--- xr_main.js
+++ xr_main.js
@@ -7,17 +7,19 @@
   const createScene = async function () {
   
       const scene = new BABYLON.Scene(engine);
-  
-      //const alpha =  3*Math.PI/2;
-      ///const beta = Math.PI/50;
-      //const radius = 220*scale;
-      const alpha =  3*Math.PI/2;
-      const beta = Math.PI/10;
-      const radius = 100*scale;
+
       const target = new BABYLON.Vector3(0, 0, 0);
-  
-      //const camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, radius, target, scene);
-      const camera = new BABYLON.ArcRotateCamera("Camera", -1, 1, radius, target, scene);
+
+      const alpha =  3*Math.PI/2;
+      const beta = Math.PI/50;
+      const radius = 220*scale;
+      const camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, radius, target, scene);
+ 
+      // const alpha =  3*Math.PI/2;
+      // const beta = Math.PI/10;
+      // const radius = 100*scale; 
+      // const camera = new BABYLON.ArcRotateCamera("Camera", -1, 1, radius, target, scene);
+
       camera.attachControl(canvas, true);
   
       const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
